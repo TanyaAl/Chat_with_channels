3ceb4325fdb53a798ac1528fb5ce75b41b534639
tatiana <alks.tatiana@gmail.com>
2025-06-04 19:39:36 +0500

    addButton

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@ start-backend:
 	npx start-server -s ./frontend/dist
 start-frontend:
 	cd frontend && npm run dev
+build:
+	cd frontend && npm ci && npm run build
  
 
 
--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -5,20 +5,26 @@ const FormPage = () => {
         <Formik initialValues={{ nikName: "", password: "" }}>
         <Form>
             <div className="form-group">
-                <label htmlFor="nikName">Email</label>
+                <label htmlFor="nikName">Ник</label>
                 <Field
-                type="nikName"
+                type="text"
                 name="nikName"
                 className="form-control"
                 />
             </div>
             <div className="form-group">
-                <label htmlFor="password">Password</label>
+                <label htmlFor="password">Пароль</label>
                 <Field
                 type="password"
                 name="password"
                 className="form-control"
                 />
+            </div>
+            <div className="form-group">
+                <button type="submit" 
+                    className="w-100 mb-3 mt-3 btn btn-outline-primary">
+                    Войти
+                </button>
             </div>
         </Form>
         </Formik>  
