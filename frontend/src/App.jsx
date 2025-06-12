a91fb3c60c857f9d87110edbd652d1a01c141bbc
tatiana <alks.tatiana@gmail.com>
2025-06-12 20:26:48 +0500

    startAth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,15 @@
 import MainPage from './pages/MainPage'
 import NotFoundPage from './pages/NotFoundPage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { StrictMode } from 'react'
-
 
 function App() {
   return (
-    <StrictMode>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />}/>
           <Route path="*" element={<NotFoundPage />}/>
         </Routes> 
       </BrowserRouter>
-    </StrictMode>
   )
 }
 
--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,12 +1,19 @@
 import { Formik, Form, Field } from 'formik';
+import { useRef, useEffect } from 'react';
 
 const FormPage = () => {
+    const inputEl = useRef(null);
+    useEffect(() => {
+        inputEl.current.focus()
+    }, []);
+    
     return (
         <Formik initialValues={{ nikName: "", password: "" }}>
         <Form>
             <div className="form-group">
                 <label htmlFor="nikName">Ник</label>
                 <Field
+                ref={inputEl}
                 type="text"
                 name="nikName"
                 className="form-control"
