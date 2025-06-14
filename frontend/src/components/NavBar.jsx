c7424909259608a7ae342a1b0b7e28677526e6e1
tatiana <alks.tatiana@gmail.com>
2025-06-14 19:07:39 +0500

    addButton

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,6 +1,8 @@
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Container, Button } from 'react-bootstrap';
+import useAuth from '../hooks/index.jsx';
 
 const NavBar = () => {
+  const auth = useAuth();
   return (
     <Navbar
       expand="lg"
@@ -8,6 +10,15 @@ const NavBar = () => {
     >
       <Container>
         <Navbar.Brand href="/">Hexlet-chat</Navbar.Brand>
+        {auth.loggedIn && (
+          <Button
+            onClick={auth.logOut}
+            type="button"
+            className="btn btn-primary ms-auto"
+          >
+            Выйти
+          </Button>
+        )}
       </Container>
     </Navbar>
   );
--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+// import { Form, Button } from 'react-bootstrap';
+// import { useFormik } from 'formik';
 
 const getAuthHeader = () => {
   const user = JSON.parse(localStorage.getItem('user'));
@@ -31,10 +33,38 @@ const ChatPage = () => {
   }, []);
   console.log('data', data);
   return (
-    <div className="text-center mb-5 mt-5">
-      {data.map((item) => {
-        return <div key={item.id}>{item.name}</div>;
-      })}
+    <div className="container h-100 my-4 overflow-hidden rounded shadow">
+      <div className="row h-100 bg-white flex-md-row">
+        <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
+          <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
+            <b>Каналы</b>
+            <button
+              type="button"
+              className="p-0 text-primary btn btn-group-vertical"
+            >
+              <span className="visually-hidden">+</span>
+            </button>
+          </div>
+          <ul
+            id="channels-box"
+            className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block"
+          ></ul>
+        </div>
+        <div className="col p-0 h-100">
+          <div className="d-flex flex-column h-100">
+            <div className="bg-light mb-4 p-3 shadow-sm small">
+              <p className="m-0">
+                <b># random</b>
+              </p>
+              <span className="text-muted">0 сообщений</span>
+            </div>
+            <div
+              id="messages-box"
+              className="chat-messages overflow-auto px-5 "
+            ></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
