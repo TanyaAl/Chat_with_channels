36691eac2884520cbc7f187464a7c79c14f5892d
tatiana <alks.tatiana@gmail.com>
2025-06-30 18:38:35 +0500

    fixPlaceholders

--- frontend/src/components/ChannelsSideBar.jsx
+++ frontend/src/components/ChannelsSideBar.jsx
@@ -32,7 +32,7 @@ const ChannelsSideBar = () => {
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
           </svg>
 
-          <span className="visually-hidden">+</span>
+          <span>+</span>
         </Button>
       </div>
       <ul
