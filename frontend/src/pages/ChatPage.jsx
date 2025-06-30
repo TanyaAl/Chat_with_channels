d21a6c7971bf1610d7524873de067089a5f1375c
tatiana <alks.tatiana@gmail.com>
2025-06-30 14:07:54 +0500

    fixText

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -5,7 +5,7 @@ import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
 import { actions as messagesActions } from '../../store/messagesSlice.js';
 import { actions as channelActions } from '../../store/channelsSlice.js';
-// import { actions as activeChannelIdActions } from '../../store/activeChannelSlice.js';
+import { actions as activeChannelIdActions } from '../../store/activeChannelSlice.js';
 import ChatMain from '../components/ChatMain.jsx';
 import Waiting from '../components/Spinner.jsx';
 import getAuthHeader from '../../utils/auth.js';
@@ -17,17 +17,19 @@ const ChatPage = () => {
 
   useEffect(() => {
     const fetchData = async () => {
+      console.log('Fetching data...');
       try {
         const responseChannels = await axios.get('/api/v1/channels', {
           headers: getAuthHeader(),
         });
         dispatch(channelActions.setChannels(responseChannels.data));
+        console.log('Loaded channels:', responseChannels.data);
         if (responseChannels.data.length > 0) {
-          // dispatch(
-          //   activeChannelIdActions.setActiveChannelId(
-          //     responseChannels.data[0].id,
-          //   ),
-          // );
+          dispatch(
+            activeChannelIdActions.setActiveChannelId(
+              responseChannels.data[0].id,
+            ),
+          );
         }
         const responseMessages = await axios.get('/api/v1/messages', {
           headers: getAuthHeader(),
--- frontend/utils/auth.js
+++ frontend/utils/auth.js
@@ -1,6 +1,9 @@
 const getAuthHeader = () => {
   const user = JSON.parse(localStorage.getItem('user'));
+  console.log('getAuthHeader - user from localStorage:', user);
   if (user && user.token) {
+    console.log('getAuthHeader - returning Authorization header with token.');
+
     return { Authorization: `Bearer ${user.token}` };
   }
   return {};
