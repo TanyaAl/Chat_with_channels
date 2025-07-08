b2929262189009ab27b746bf857ab899e1acf35e
tatiana <alks.tatiana@gmail.com>
2025-07-08 20:38:11 +0500

    fixCache

--- frontend/src/components/modals/index.js
+++ frontend/src/components/modals/index.js
@@ -1,11 +1,6 @@
-// import AddChannel from './AddChannel.jsx'
-// import RemoveChannel from './RemoveChannel.jsx'
-// import RenameChannel from './RenameChannel.jsx'
-import { lazy } from 'react'
-
-const AddChannel = lazy(() => import('./AddChannel.jsx'))
-const RemoveChannel = lazy(() => import('./RemoveChannel.jsx'))
-const RenameChannel = lazy(() => import('./RenameChannel.jsx'))
+import AddChannel from './AddChannel.jsx'
+import RemoveChannel from './RemoveChannel.jsx'
+import RenameChannel from './RenameChannel.jsx'
 
 const modals = {
   adding: AddChannel,
