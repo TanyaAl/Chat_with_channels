7f767f8777c6b1c1c8bd35c40e5c9c7490eb17c0
tatiana <alks.tatiana@gmail.com>
2025-07-08 13:19:20 +0500

    fixRollbar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,37 +9,38 @@ import { init } from './init.jsx'
 import { I18nextProvider } from 'react-i18next'
 import { i18next } from './init.jsx'
 import { ToastContainer, Bounce } from 'react-toastify'
-// import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react'
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react'
 
 const { socket } = init()
 
-// // const rollbarConfig = {
-//   accessToken: import.meta.env.VITE_ROLLBAR_TOKEN,
-//   environment: 'prod',
-// }
-// console.log('Rollbar token:', import.meta.env.VITE_ROLLBAR_TOKEN)
+const rollbarConfig = {
+  accessToken: import.meta.env.VITE_ROLLBAR_TOKEN,
+  environment: import.meta.env.MODE,
+}
+console.log('Rollbar token:', import.meta.env.VITE_ROLLBAR_TOKEN)
+console.log('RollbarEnv:', import.meta.env.MODE)
 
 createRoot(document.getElementById('root')).render(
-  // <RollbarProvider config={rollbarConfig}>
-  // <ErrorBoundary>
-  <Provider store={store}>
-    <I18nextProvider i18n={i18next}>
-      <App socket={socket} />
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick={false}
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-        transition={Bounce}
-      />
-    </I18nextProvider>
-  </Provider>,
-  //   </ErrorBoundary>
-  // </RollbarProvider>,
+  <RollbarProvider config={rollbarConfig}>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <I18nextProvider i18n={i18next}>
+          <App socket={socket} />
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick={false}
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+            transition={Bounce}
+          />
+        </I18nextProvider>
+      </Provider>
+    </ErrorBoundary>
+  </RollbarProvider>,
 )
